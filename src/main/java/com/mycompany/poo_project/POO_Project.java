9a59cd1fe1dfac671d25a9cb20b476dbe63bcf78
grand <grand@192.168.1.25>
2023-11-11 21:40:06 +0100

    Création du projet

---
+++ src/main/java/com/mycompany/poo_project/POO_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo_project;
+
+/**
+ *
+ * @author grand
+ */
+public class POO_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
